99c356fb3a66b84718d30a283b9aeb2827da3035
Shae-Marie Small <shaemarie.small@gmail.com>
2021-11-28 01:45:03 -0400

    Added half working files

--- connection.php
+++ connection.php
@@ -2,12 +2,16 @@
 
 $host = "localhost";  
 $user = "project2admin";  
-$password = "admin123";  
+$password = "admi123";  
 $db_name = "BugMe";  
 
-$con = new PDO("mysql:host=$host;db_name=$db_name;", $user, $password);
-//mysqli_connect($host, $user, $password, $db_name);  
-    if(mysqli_connect_errno()) {  
-        die("Failed to connect with MySQL: ". mysqli_connect_error());  
-    }  
+
+try{
+    $con = new PDO("mysql:host=$host;db_name=$db_name;", $user, $password);
+    // Set the PDO error mode to exception
+    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  } catch(PDOException $e){
+    die("ERROR: Could not connect. " . $e->getMessage());
+
+}  
 ?>  
